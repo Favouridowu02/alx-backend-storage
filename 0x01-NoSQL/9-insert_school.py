1e0bd452cefafdeeb95a122da8548c84dce92adf
Favour Idowu <favouridowu02@gmail.com>
2024-10-23 19:58:53 +0100

    Add a file that contains a Python function that inserts a new document in a collection based on kwargs:
    Prototype: def insert_school(mongo_collection, **kwargs):
    mongo_collection will be the pymongo collection object
    Returns the new _id

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -11,5 +11,5 @@ def insert_school(mongo_collection, **kwargs):
         A  function that inserts a new document in a collection based
         on kwargs
     """
-    mongo_collection.insert_one(locals()["kwargs"])
-    return mongo_collection.insert_id
+    new_obj = mongo_collection.insert_one(kwargs)
+    return new_obj.inserted_id
