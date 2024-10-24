8ed7f494ad0a7d83df760d145e21e535931cb316
Favour Idowu <favouridowu02@gmail.com>
2024-10-24 12:45:29 +0100

    Add  a Python function that changes all topics of a school document based on the name:
    Prototype: def update_topics(mongo_collection, name, topics):
    mongo_collection will be the pymongo collection object
    name (string) will be the school name to update
    topics (list of strings) will be the list of topics approached in the school

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    This Module contains a function that changes all topics of a school
+    document based on the name
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+        This Module contains a Python function that changes all topics
+        of a school document based on the name
+
+        Arguments:
+            mongo_collection: a pymongo collection object
+            name: The school name to update
+            topics: a List of Topics approached in the school
+    """
+    mongo_collection.update_many(
+            {'name': name},
+            { '$set': {'topics': topics}}
+    )
+    #return new_obj
