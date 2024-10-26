d56bdc916d7a889cec8b77c1e017c30fcd56195a
Favour Idowu <favouridowu02@gmail.com>
2024-10-26 18:30:09 +0100

    Add a function get, get_str, get_int
    Implemented a function get():  method that take a key string argument and an optional Callable argument named fn. This callable will be used to convert the data back to the desired format.
    
    Implemented a function get_int(): a method that accepts a string that is the key returns an integer value of the data
    
    Implemented a function get_str(): a method that accepts a string that is the key and returns a string value of the data

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@
 """
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -33,3 +33,19 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None
+            ) -> Union[str, bytes, int, float]:
+        """
+            This method is used to get the value from the redis db
+        """
+        data = self._redis.get(key)
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        return self._redis.get(key), lambda x: x.decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        return self._redis.get(key, int)
