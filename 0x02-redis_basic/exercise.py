8a4860d78473fd7948b3f728da1053ee736bf42c
Favour Idowu <favouridowu02@gmail.com>
2024-10-26 17:22:56 +0100

    Add a file that contains a class Cache
    Implemented a class Cache. This class makes use of redis to store data when the method store which accepts an argument of data(str|int|bytes
    |float) and returns the key use to store the data

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+    This Module contains a class Cache.
+"""
+import redis
+from uuid import uuid4
+
+
+class Cache:
+    """
+        This Class is a Cache class
+
+        Methods:
+            store: this method is used to store data
+    """
+    def __init__(self):
+        """
+            This is the initialization of the class
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb
+
+    def store(self, data: str | bytes | int | float) -> str:
+        """
+            This method takes an argument and returns a string
+
+            Arguments:
+                data: The data to be stored
+
+            Return: returns the random key used for storing
+        """
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
